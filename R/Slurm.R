e84a3c795b8756e6c7438e961800919a3e6b153a
Chris Scheller <schelcj@pobox.com>
2013-10-14 12:56:35 -0400

    getting a handle on the unit tests

--- R/Slurm.R
+++ R/Slurm.R
@@ -3,26 +3,24 @@
 # SLURM_ARRAY_TASK_ID
 # 
 # SLURM_ARRAYID
-
-
-Sys.getenv('SLURM_ARRAYID')
+# Sys.getenv('SLURM_ARRAYID')
 
 slurm <- function() {
   UseMethod("slurm")
 }
 
 slurm.jobid <- function() {
-  return as.numeric(Sys.getenv('SLURM_JOBID'))
+  return(as.numeric(Sys.getenv('SLURM_JOBID')))
 }
 
 slurm.array_job_id <- function() {
-  return as.numberic(Sys.getenv('SLURM_ARRAY_JOB_ID'))
+  return(as.numberic(Sys.getenv('SLURM_ARRAY_JOB_ID')))
 }
 
 slurm.array_task_id <- function() {
-  return as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
+  return(as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID')))
 }
 
 slurm.arrayid <- function() {
-  return as.numeric(Sys.getenv('SLURM_ARRAYID'))
+  return(as.numeric(Sys.getenv('SLURM_ARRAYID')))
 }
